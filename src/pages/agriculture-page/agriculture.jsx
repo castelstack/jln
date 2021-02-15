90a3c2f02aa5e1d49cc8d66862bbd3103d9df2c1
castelstack <okaforhenry01@gmail.com>
2021-02-15 09:24:31 +0100

    page with one template to populate

--- src/pages/agriculture-page/agriculture.jsx
+++ src/pages/agriculture-page/agriculture.jsx
@@ -1,39 +1,20 @@
 import { Divider } from "@material-ui/core";
 import React from "react";
 import Box from "../../components/box/box";
-import { PagesBox, RevPagesBox } from "../../components/box/pages-box";
+import { PagesBox } from "../../components/box/pages-box";
 import Img from "../../images/slide2.jpg";
 const AgriculturePage = () => {
-  
   return (
     <div>
       <Box title='Agriculture' currentPage='Agriculture' />
       <PagesBox
         title='LIVESTOCK'
         description='loremDonec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'
-      alt='construction'
-      image={Img}
-      offers={[ 'FISH FARM-','POULTRY-','PIG FARM-','SNAIL-','MUSHROOM']}
-          />
-          <Divider light />
-          <RevPagesBox  title='WE PRODUCE THE FRESHEST FRUITS.'
-        description='loremDonec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'
-      alt='construction'
-      image={Img}
-          />
-          <Divider light />
-          <PagesBox
-        title='FARM CROP'
-        description='loremDonec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'
-      alt='construction'
-      image={Img}
-          />
-          <Divider light />
-          <RevPagesBox  title='PROCESSING'
-        description='loremDonec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'
-      alt='construction'
-      image={Img}
-          />
+        alt='construction'
+        image={Img}
+        offers={["FISH FARM-", "POULTRY-", "PIG FARM-", "SNAIL-", "MUSHROOM"]}
+      />
+      <Divider light />
     </div>
   );
 };
